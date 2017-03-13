6a70fbc43199fd7cde8ebc781f58f438851ef130
ldw-sh-cn <mr.david.liang@foxmail.com>
2017-03-14 00:09:02 +0800

    init project

---
+++ vuedemo/app/assets/js/app.js
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+define(function(require, exports, module) {
+	var $ = require('jquery');
+	require('vue');
+	require('vue-router');
+
+	var app = new Vue({
+		el: '#app',
+		data: {
+			message: 'Hello Vue!'
+		}
+	})
+
+});
---
+++ vuedemo/app/assets/js/lib/config.js
@@ -0,0 +1,12 @@
+/**
+ *
+ */
+seajs.config({
+	base: '/',
+	alias: {
+		'jquery': 'assets/js/lib/jquery-1.11.0',
+		'vue': 'assets/js/lib/vue',
+		'vue-router': 'assets/js/lib/vue-router'
+	},
+	charset: 'utf-8'
+});
